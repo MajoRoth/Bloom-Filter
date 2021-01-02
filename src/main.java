4775d512af1d4154c556edbfa362444f0e298f54
Ido Loven <idoloven123@gmail.com>
2021-01-03 01:34:31 +0200

    Receiving input from file

--- src/main.java
+++ src/main.java
@@ -1,15 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class main {
     public static void main(String args[]){
-        int m = 100;
-        int k = 3;
+        /*
+         * setting the constants as in the example in the task
+         */
+    	
+    	String[] input_to_insert = read_file("C:\\Users\\idoloven\\desktop\\all.txt");
+        int N = input_to_insert.length;
+    	int m = 32*N;
+        int k = 13;
+        
+        /*
+         * creating the table and inserting the elements
+         */
         BelongTable t = new BelongTable(m, k);
-        t.add_element("hey");
-        t.add_element("cow");
-        t.add_element("milk");
-        System.out.println(t.is_belong("hey"));
-
+        for(int i=0; i<N; i++) {
+        	t.add_element(input_to_insert[i]);
+        }
+        
+        
+        String[] input_to_check = read_file("C:\\Users\\idoloven\\desktop\\evens.txt");
+        for(int j=0; j<input_to_check.length; j++) {
+            System.out.println(t.is_belong(input_to_check[j]));
+        }
+        }
+        
+    
+    
+    private static String[] read_file(String path) {
+    	/*
+         * receiving input from a file
+         */
+    	String file = "";
+        try(BufferedReader in = new BufferedReader(new FileReader(path))) {
+            String str;
+            while ((str = in.readLine()) != null) {
+            	file += str;
+            }
+        }
+        catch (IOException e) {
+            System.out.println("File Read Error");
+        }
+        
+        /*
+         * converting the comma-separated values to a String array
+         */
+        String[] input = file.split(",");
+        return input;
+    	
     }
-    /*
-     * changed
-     */
-}
+    }
